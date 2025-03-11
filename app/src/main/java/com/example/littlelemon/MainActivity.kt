36271e3a4a8a0f9f61e52ac2397f9548f0034499
Gideon <gideon.tobing@gmail.com>
2025-03-11 17:12:21 +1000

    building ui composable

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -3,18 +3,58 @@ package com.example.littlelemon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Text(text = "Little Lemon",
-                fontSize = 32.sp,
-                color = Color(0xFFF4CE14)
-            )
+            Column(
+                Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Little Lemon",
+                    fontSize = 32.sp,
+                    color = Color(0xFFF4CE14)
+                )
+                Text(
+                    text = stringResource(id = R.string.chicago),
+                    fontSize = 32.sp,
+                    color = Color(0xFFF4CE14)
+                )
+                Row {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        border = BorderStroke(1.dp, Color.Red),
+                        shape = RoundedCornerShape(10.dp),
+                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Gray)
+                    ) {
+                        Text(text = stringResource(id = R.string.order))
+                    }
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_android_black_24dp),
+                        contentDescription = ""
+                    )
+                }
+            }
         }
     }
 }
