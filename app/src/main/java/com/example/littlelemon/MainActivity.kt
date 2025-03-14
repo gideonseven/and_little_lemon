fa735414edaa9a593d39f5ce3e9577878f780d56
Gideon <gideon.tobing@gmail.com>
2025-03-14 16:30:33 +1000

    styling with modifier

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -5,14 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,36 +28,48 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+            MainComponent()
+        }
+    }
+}
+
+@Composable
+fun MainComponent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0XFF495E57)),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start,
+    ) {
+        Text(
+            text = "Little Lemon",
+            fontSize = 32.sp,
+            color = Color(0xFFF4CE14),
+            modifier = Modifier.padding(start = 20.dp, top = 20.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.chicago),
+            fontSize = 24.sp,
+            color = Color(0xFFFFFFFF),
+            modifier = Modifier.padding(start = 20.dp)
+        )
+        Row(
+            Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                border = BorderStroke(1.dp, Color.Red),
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Gray)
             ) {
-                Text(
-                    text = "Little Lemon",
-                    fontSize = 32.sp,
-                    color = Color(0xFFF4CE14)
-                )
-                Text(
-                    text = stringResource(id = R.string.chicago),
-                    fontSize = 32.sp,
-                    color = Color(0xFFF4CE14)
-                )
-                Row {
-                    Button(
-                        onClick = { /*TODO*/ },
-                        border = BorderStroke(1.dp, Color.Red),
-                        shape = RoundedCornerShape(10.dp),
-                        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Gray)
-                    ) {
-                        Text(text = stringResource(id = R.string.order))
-                    }
-                    Image(
-                        painter = painterResource(id = R.drawable.ic_android_black_24dp),
-                        contentDescription = ""
-                    )
-                }
+                Text(text = stringResource(id = R.string.order))
             }
+            Image(
+                painter = painterResource(id = R.drawable.ic_android_black_24dp),
+                contentDescription = ""
+            )
         }
     }
 }
