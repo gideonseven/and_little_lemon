936515c0cc14db9d030dfd9d67ee8d28abfd6704
Gideon <gideon.tobing@gmail.com>
2025-03-14 16:43:58 +1000

    modifiers with layout

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -10,7 +10,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -18,6 +21,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -37,7 +41,7 @@ class MainActivity : ComponentActivity() {
 fun MainComponent() {
     Column(
         modifier = Modifier
-            .fillMaxSize()
+            .fillMaxWidth()
             .background(Color(0XFF495E57)),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start,
@@ -55,21 +59,35 @@ fun MainComponent() {
             modifier = Modifier.padding(start = 20.dp)
         )
         Row(
-            Modifier.fillMaxSize(),
+            Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
             horizontalArrangement = Arrangement.Center
         ) {
-            Button(
-                onClick = { /*TODO*/ },
-                border = BorderStroke(1.dp, Color.Red),
-                shape = RoundedCornerShape(10.dp),
-                colors = ButtonDefaults.buttonColors(backgroundColor = Color.Gray)
-            ) {
-                Text(text = stringResource(id = R.string.order))
-            }
-            Image(
-                painter = painterResource(id = R.drawable.ic_android_black_24dp),
-                contentDescription = ""
+
+            Text(
+                text = stringResource(id = R.string.description_one),
+                Modifier.width(200.dp),
+                color = Color.White,
+                fontSize = 21.sp
             )
+
+            Image(
+                painter = painterResource(id = R.drawable.restaurant_food),
+                contentDescription = "",
+                Modifier
+                    .height(200.dp)
+                    .clip(RoundedCornerShape(20.dp))
+            )
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            border = BorderStroke(1.dp, Color.Red),
+            shape = RoundedCornerShape(10.dp),
+            colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFFF4CE14)),
+            modifier = Modifier.padding(horizontal = 20.dp)
+        ) {
+            Text(text = stringResource(id = R.string.order))
         }
     }
 }
